656e03d73acf5fb80fb60f8a8f1432fcf2696582
Vita <dragic.hsieh@gmail.com>
2022-12-06 01:34:04 +0800

    add navbar component

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,7 +1,28 @@
+import { Button, Container, Form, Navbar as BootstrapNavbar } from 'react-bootstrap';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 
 const Navbar: NextPage = () => {
-	return <div>Navbar</div>;
+	return (
+		<BootstrapNavbar bg='light' fixed='top'>
+			<Container fluid>
+				<BootstrapNavbar.Brand href='#home'>Foddy Dao</BootstrapNavbar.Brand>
+				<BootstrapNavbar.Toggle />
+				<BootstrapNavbar.Collapse className='justify-content-end'>
+					<Form className='d-flex me-2'>
+						<Form.Control
+							type='search'
+							placeholder='Search Restaurant'
+							className='me-2'
+							aria-label='Search'
+						/>
+						<Button variant='outline-primary'>Search</Button>
+					</Form>
+					<ConnectButton />
+				</BootstrapNavbar.Collapse>
+			</Container>
+		</BootstrapNavbar>
+	);
 };
 
 export default Navbar;
