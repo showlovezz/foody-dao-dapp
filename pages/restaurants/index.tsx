0489c0288ced2202582fe666a0cf9b3b96047a5f
Vita <dragic.hsieh@gmail.com>
2022-12-09 10:53:21 +0800

    add restaurants page

--- pages/restaurants/index.tsx
+++ pages/restaurants/index.tsx
@@ -1,7 +1,28 @@
+import { Col, Container, Row } from 'react-bootstrap';
 import type { NextPage } from 'next';
 
+import RestaurantCard from '../../components/RestaurantCard';
+
+import useFetchRestaurantList from '../../hooks/useFetchRestaurantList';
+
 const Restaurants: NextPage = () => {
-	return <div>餐廳</div>;
+	const { restaurantList } = useFetchRestaurantList();
+
+	return (
+		<article>
+			<Container>
+				<section>
+					<Row>
+						<Col md={{ span: 6, offset: 3 }}>
+							{restaurantList.map((restaurant) => (
+								<RestaurantCard key={restaurant.restaurant_id} restaurant={restaurant} />
+							))}
+						</Col>
+					</Row>
+				</section>
+			</Container>
+		</article>
+	);
 };
 
 export default Restaurants;
